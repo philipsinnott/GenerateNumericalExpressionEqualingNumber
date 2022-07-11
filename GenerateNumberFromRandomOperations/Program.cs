4a89a9c069b2c9630778f3bd7290be145d58f937
Philip Sinnott <philipsinnotts@gmail.com>
2022-07-11 22:11:36 +0200

    Added method for generating fixed sets of random numbers

--- GenerateNumberFromRandomOperations/Program.cs
+++ GenerateNumberFromRandomOperations/Program.cs
@@ -6,12 +6,27 @@ namespace GenerateNumberFromRandomOperations // Note: actual namespace depends o
     {
         static void Main()
         {
-            Console.WriteLine("TEST");
+            int[] setOfRandomNums = GenerateSetOfRandomNumbers(10);
+            //Console.WriteLine(setOfRandomNums[4]);
+            foreach (int i in setOfRandomNums)
+                Console.WriteLine(i);
         }
 
         /// Generate a specific number from a set of random numbers and a set of random operations ///
         /// Example 1 | Input: 3 + 2 * 2 - 6 | Output: 1
         /// Example 2 | Input: 7 - 9 * 6 - 3 | Output: -50
         /// Example 3 | Input: 9 - 0 + 0 * 5 | Output: 9
+        /// 
+
+        static int[] GenerateSetOfRandomNumbers(int amount)
+        {
+            int[] setRandomNums = new int[amount];
+            Random random = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                setRandomNums[i] = random.Next(0, 10);
+            }
+            return setRandomNums;
+        }
     }
 }
