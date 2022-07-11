38dae6892d2f7cdfb28aa91be03bd3fe80961fd9
Philip Sinnott <philipsinnotts@gmail.com>
2022-07-11 21:56:19 +0200

    Add project files.

---
+++ GenerateNumberFromRandomOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenerateNumberFromRandomOperations // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("TEST");
+        }
+
+
+    }
+}
