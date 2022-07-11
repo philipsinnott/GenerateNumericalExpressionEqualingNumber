b49f2e7860c985148605b8935fc7e1ba57095dd8
Philip Sinnott <philipsinnotts@gmail.com>
2022-07-11 21:57:44 +0200

    Example I/O

--- GenerateNumberFromRandomOperations/Program.cs
+++ GenerateNumberFromRandomOperations/Program.cs
@@ -9,6 +9,9 @@ namespace GenerateNumberFromRandomOperations // Note: actual namespace depends o
             Console.WriteLine("TEST");
         }
 
-
+        /// Generate a specific number from a set of random numbers and a set of random operations ///
+        /// Example 1 | Input: 3 + 2 * 2 - 6 | Output: 1
+        /// Example 2 | Input: 7 - 9 * 6 - 3 | Output: -50
+        /// Example 3 | Input: 9 - 0 + 0 * 5 | Output: 9
     }
 }
